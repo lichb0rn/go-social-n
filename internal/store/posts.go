c5352dd532c7812f2e3eb19fa8aca6d87b83c497
Miroslav Taleiko <mtaleiko@gmail.com>
2025-03-28 09:37:14 +0300

    feat: add pagination

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -147,7 +147,7 @@ func (s *PostStore) Update(ctx context.Context, post *Post) error {
 	return nil
 }
 
-func (s *PostStore) GetUserFeed(ctx context.Context, userId int64) ([]*PostWithMetadata, error) {
+func (s *PostStore) GetUserFeed(ctx context.Context, userId int64, fq PaginatedFeedQuery) ([]*PostWithMetadata, error) {
 	query := `
 	SELECT p.id,p.user_id, p.title, p.content, p.created_at, p.version, p.tags, u.username,
 	COUNT(c.id) AS comment_count
@@ -157,13 +157,14 @@ func (s *PostStore) GetUserFeed(ctx context.Context, userId int64) ([]*PostWithM
 	JOIN followers f ON f.follower_id = p.user_id OR p.user_id = $1
 	WHERE f.user_id = $1 OR p.user_id = $1
 	GROUP BY p.id, u.username
-	ORDER BY p.created_at DESC
+	ORDER BY p.created_at ` + fq.Sort + `
+	LIMIT $2 OFFSET $3
 	`
 
 	ctx, cancel := context.WithTimeout(ctx, DBQueryTimeoutDuration)
 	defer cancel()
 
-	rows, err := s.db.QueryContext(ctx, query, userId)
+	rows, err := s.db.QueryContext(ctx, query, userId, fq.Limit, fq.Offset)
 	if err != nil {
 		return nil, err
 	}
